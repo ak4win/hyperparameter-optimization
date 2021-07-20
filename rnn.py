3a632c01110eb970bab7451feb4b6fda55a4ee63
Alexander Kuebel <alexander.kuebel@code.berlin>
2021-07-20 11:48:09 -0400

    add rnn model

---
+++ rnn.py
@@ -0,0 +1,57 @@
+from numpy.random import seed
+
+seed(1)
+
+# Tensorflow imports
+from tensorflow.keras.layers import LSTM, Input, RepeatVector
+from tensorflow.keras.models import Model
+
+
+def create_model(
+    sequence_length: int,
+    n_dims: int,
+    batch_size: int,
+    activation: str = "tanh",
+    recurrent_activation: str = "sigmoid",
+    # kernel_regularizer=l2(0.0),
+    # bias_regularizer=l2(0.0),
+    # activity_regularizer=l2(0.0),
+    dropout: float = 0.0,
+    recurrent_dropout: float = 0.0,
+):
+    # [batch, timesteps, feature] is shape of inputs
+    inputs: Input = Input(shape=(sequence_length, n_dims), batch_size=batch_size)
+    x = inputs
+    x = LSTM(
+        n_dims,
+        return_sequences=False,
+        stateful=True,
+        activation=activation,
+        recurrent_activation=recurrent_activation,
+        # kernel_regularizer=kernel_regularizer,
+        # bias_regularizer=bias_regularizer,
+        # activity_regularizer=activity_regularizer,
+        dropout=dropout,
+        recurrent_dropout=recurrent_dropout,
+    )(x)
+    x = RepeatVector(sequence_length)(x)
+    x = LSTM(
+        n_dims,
+        stateful=True,
+        return_sequences=True,
+        # bias_initializer=global_mean_bias,
+        unit_forget_bias=False,
+        activation=activation,
+        recurrent_activation=recurrent_activation,
+        # kernel_regularizer=kernel_regularizer,
+        # bias_regularizer=bias_regularizer,
+        # activity_regularizer=activity_regularizer,
+        dropout=dropout,
+        recurrent_dropout=recurrent_dropout,
+    )(x)
+    outputs = x
+
+    model = Model(inputs, outputs)
+    model.compile(optimizer="adam", loss="mse")
+    model.summary()
+    return model
